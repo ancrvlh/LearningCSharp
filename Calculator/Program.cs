b1b0ca09ba1e8a8609957f356c1e9b832bf22207
ancrvlh <abraaoncrvlh@gmail.com>
2022-07-18 11:13:08 -0300

    Calculator in C#

---
+++ Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    Console.Clear();
+    Console.Write("Enter first number: ");
+    float n1 = float.Parse(Console.ReadLine());
+
+    Console.Write("Enter second number: ");
+    float n2 = float.Parse(Console.ReadLine());
+
+    float result = n1 + n2;
+
+    Console.Write($"Result is: {result}");
+  }
+}
