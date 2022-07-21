508a8eb603907c8b5a9d7f5ec66260ee231f6caa
ancrvlh <abraaoncrvlh@gmail.com>
2022-07-21 15:50:50 -0300

    Apresentation C#

---
+++ Apresentation/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program {
+  static void Main(String[] args) {
+    Age();
+  }
+  static void Age() {
+    Console.Clear();
+
+    Console.Write("Enter your name: ");
+    string name = Console.ReadLine();
+
+    Console.Write("Enter year: ");
+    int year = int.Parse(Console.ReadLine());
+
+    int age = 2022 - year;
+    Console.WriteLine($"Your name is: {name} and your age is: {age} years old.");
+
+    if (age >= 18) {
+      Console.WriteLine("You old man.");
+    } else {
+      Console.WriteLine("You new.");
+    }
+  }
+}
